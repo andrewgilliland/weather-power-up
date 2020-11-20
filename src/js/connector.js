8b6947b66f1a8546b4c9f3fea1e87cbdd7f6c5e7
Andrew Gilliland <andrewpgilliland@gmail.com>
2020-11-20 10:58:58 -0600

    added trello connector script

---
+++ src/js/connector.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+window.TrelloPowerUp.intitialize({
+  "card-badges": function (t, opts) {
+    // return an array of card badges for the given card
+    return [];
+  },
+});
