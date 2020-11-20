8d692774f131fbaae6ef6baec0890fac20787011
Andrew Gilliland <andrewpgilliland@gmail.com>
2020-11-20 12:23:18 -0600

    load card for card-badges capability

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,15 @@
 console.log("Hello World");
 
-window.TrelloPowerUp.intitialize({
+window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     // return an array of card badges for the given card
-    return [];
+    return t.card("all").then(function (card) {
+      console.log(card);
+      return [
+        {
+          text: card.idShort,
+        },
+      ];
+    });
   },
 });
